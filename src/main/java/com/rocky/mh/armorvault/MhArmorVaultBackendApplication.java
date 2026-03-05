b9084625f379cb232d08169595f864158409fc19
RockyChung <mrrockychung@gmail.com>
2026-03-05 10:52:05 +0800

    初始化

---
+++ src/main/java/com/rocky/mh/armorvault/MhArmorVaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rocky.mh.armorvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhArmorVaultBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhArmorVaultBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rocky/mh/armorvault/MhArmorVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocky.mh.armorvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhArmorVaultBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
